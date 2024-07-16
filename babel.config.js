16c4a71f5ecaf7efaae61ade01a2d6d4d389bfba
EJacoby515 <ejacobowitz515@gmail.com>
2024-07-16 15:53:16 -0400

    modified dependencies to launch on amplify

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,8 @@ module.exports = function(api) {
   return {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
-      ['module:react-native-dotenv']
+      ['module:react-native-dotenv'],
+      ['@babel/plugin-proposal-private-methods', { "loose": true }]
     ]
   };
 }
